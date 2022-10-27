5d21a9f139cccf0f7cc7c58df3e57dd4a63d47c5
cporcellato <claudia.porcellato@gmail.com>
2022-10-27 22:33:43 +0200

    Removed useless files

--- django_openedu/edu_data/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# These models are registered to be able to see and edit them in the admin website
-
-from .models import EduMaterial
-admin.site.register(EduMaterial)
