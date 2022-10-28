5a6eda04e73d52548d00da91cb61eda09b658c52
cporcellato <claudia.porcellato@gmail.com>
2022-10-28 12:48:22 +0200

    New django db structure

--- django_openedu/edu_data/models.py
+++ django_openedu/edu_data/models.py
@@ -5,9 +5,37 @@ from django.urls import  reverse
 # id is not required, given that one imports from django model
 
 # Model that contains the new educational material entries
+class Author(models.Model):
+    Name=models.CharField(max_length=40)
+    Email=models.EmailField(max_length=40)
+    Phone=models.CharField(max_length=40, blank = True, null = True)
+    Website_url=models.URLField(max_length=300, blank = True, null = True)
+
+class Publisher(models.Model):
+    Name=models.CharField(max_length=40)
+    Website_url=models.URLField(max_length=300)
+
+
+class Media_Channel(models.Model):
+    Name=models.CharField(max_length=50)
+    Website_url=models.URLField(max_length=300)
+
+
+class Institution(models.Model):
+    Name=models.CharField(max_length=50)
+    Website_url=models.URLField(max_length=300)
+    id_media_channel = models.ForeignKey(Media_Channel, on_delete=models.PROTECT, blank = True, null = True)
+
+
 class EduMaterial(models.Model):
     title = models.CharField(max_length=120)  # we can change this default to a larger value if required
     description = models.TextField()
+    id_author = models.ForeignKey(Author, on_delete=models.PROTECT, blank = True, null = True)
+    id_publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, blank = True, null = True)
+    id_Media_Channel = models.ForeignKey(Media_Channel, on_delete=models.PROTECT, blank = True, null = True)
+    id_Institution = models.ForeignKey(Institution, on_delete=models.PROTECT, blank=True, null=True)
+
+
 
     def __str__(self):
         return self.title  # this is the return one gets when referenced to this model
@@ -28,3 +56,7 @@ class EduMaterial_Categories(models.Model):
     # educational material is deleted
     id_category = models.ForeignKey(Categories, on_delete=models.CASCADE)  # cascade, for deleting the entry if the
     # category is deleted
+
+
+
+
