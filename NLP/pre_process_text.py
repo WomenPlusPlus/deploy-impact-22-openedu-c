16c83c96e285d6a2928af9694e5e7aff86d3f120
kezihlmann <kevin.zihlmann@bluewin.ch>
2022-10-28 22:33:53 +0800

    added the pre_processing of text. bi_encoder runs very fast now, cross_encoder runs but is slow.
    TODO: implement tf_idf method and connect to the azure server

---
+++ NLP/pre_process_text.py
@@ -0,0 +1,24 @@
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+import re
+
+def pre_process_text(doc):
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    # lower case and remove special characters\whitespaces
+    doc = re.sub(r'[^a-zA-Z\s]', '', doc, flags=re.I|re.A) # [^a-zA-Z\s] => remove any digits, special characters, symbols etc.
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = nltk.word_tokenize(doc)
+    # filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
+# sentence = "These are two test sentences to check if this is working correctly. We do this 4 social good. "
+# print(sentence)
+# print(pre_process_text(sentence))
