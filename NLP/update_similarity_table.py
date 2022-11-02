5963037522f182b79ace98889b824efe5ac3f304
kezihlmann <kevin.zihlmann@bluewin.ch>
2022-11-02 23:40:55 +0900

    prepare code for claudia

--- NLP/update_similarity_table.py
+++ NLP/update_similarity_table.py
@@ -28,6 +28,7 @@ def update_similarity_table():
     METHOD = "BI_ENCODER" # choose between BI_ENCODER, CROSS_ENCODER and TF_IDF
 
     LOAD_SIMILARITIES = False
+    LOAD_FROM_NEW_DB = True
 
     SAVE_SIMILARITIES = True
 
@@ -40,13 +41,13 @@ def update_similarity_table():
         exit()
 
     # query the text information of all projects
-    engine = sa.create_engine('postgresql://deploy_impact:AVNS_tEdPMnvmmI0knrjJe-R@deploy-impact-cg-chrisg-demo.aivencloud.com:24947/openedu')
-    df_1 = pd.read_sql_query('SELECT title_en FROM sito_project', engine)
-    df_2 = pd.read_sql_query('SELECT subtitle_en FROM sito_project', engine)
-    df_3 = pd.read_sql_query('SELECT short_description_en FROM sito_project', engine) #TODO: remove css format
-    df_4 = pd.read_sql_query('SELECT more_information_en FROM sito_project', engine) #TODO: remove css format
-    df_combined = pd.concat([df_1, df_2, df_3], axis=1, join='inner')
-    df_text = df_combined.apply(' '.join, axis=1)
+    if LOAD_FROM_NEW_DB:
+        # TODO
+        pass
+    else:
+        engine = sa.create_engine('postgresql://deploy_impact:AVNS_tEdPMnvmmI0knrjJe-R@deploy-impact-cg-chrisg-demo.aivencloud.com:24947/openedu')
+        df = pd.read_sql_query('SELECT title_en, subtitle_en, short_description_en FROM sito_project', engine) 
+        df_text = df.apply(' '.join, axis=1)
 
     n = len(df_text)
 
@@ -101,6 +102,8 @@ def update_similarity_table():
         # TODO: delete the outdated table
         n_related = 3
         # update the table in the database
+        # TODO connect to the server
+
         for i in range(n):
             list_to_sort = np.zeros((n, 2))
             list_to_sort[:,0] = range(n)
