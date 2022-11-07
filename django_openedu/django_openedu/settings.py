6f76a1ea0225deed2732bfde69a0648c1c677631
DanielaPavia <113996689+DanielaPavia@users.noreply.github.com>
2022-11-07 13:27:31 +0100

    Update settings.py

--- django_openedu/django_openedu/settings.py
+++ django_openedu/django_openedu/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # azure
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
