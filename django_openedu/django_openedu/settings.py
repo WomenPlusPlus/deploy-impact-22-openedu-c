e8af42912f070d0ea31acf0bec5075ab4c3e520c
DanielaPavia <113996689+DanielaPavia@users.noreply.github.com>
2022-11-08 10:32:59 +0100

    Update settings.py

--- django_openedu/django_openedu/settings.py
+++ django_openedu/django_openedu/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'openedu-groupc.azurewebsites.net',
+    #'127.0.0.1',
+    #'openedu-groupc.azurewebsites.net',
     'openeduc.azurewebsites.net'
 ]
 
