started with the implementation of the semantic_search function. the masking is not correct yet (line 29)

NLP/get_similarities.py
@@ -18,8 +18,8 @@ def get_similarities_using_bi_encoder(df_text, df_id):
     ids = df_id.to_numpy()
     print(sentence_emb.shape)
     print(ids.shape)
-    sentence_emb.tofile("NLP/embedings.npy")
-    ids.tofile("NLP/ids.npy")
+    np.save("NLP/embeddings", sentence_emb)
+    np.save("NLP/ids", ids)
 
     # Cosine Similarity
     similarity_score = cosine_similarity(sentence_emb)

NLP/semantic_search.py
@@ -1,15 +1,31 @@
+import numpy as np  
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
 def semantic_search(search_key):
     """
     Arguement: search_key - String with the word that the user wants to do the search
     Return argument: ids - a list of project id's that need will be displayed to user
     """ 
     threshold = 0.25
-    # loading the embedings and the ids from binary file
+    path_embeddings = "NLP/embeddings.npy"
+    path_ids = "NLP/ids.npy"
+    # loading the embeddings and the ids from binary file
+    project_embeddings = np.load(path_embeddings, allow_pickle=True)
+    ids = np.load(path_ids, allow_pickle=True)
 
-    # computing embeding of search_key
+    # Load the pre-trained model
+    model = SentenceTransformer('stsb-mpnet-base-v2')
+
+    # computing embedding of search_key
+    search_key_embedding = model.encode(search_key, show_progress_bar=False)
+    search_key_embedding = search_key_embedding.reshape(1, -1)
+
 
     # compute cosine similarity
+    similarity_score = cosine_similarity(project_embeddings, search_key_embedding)
 
     # find ids where similarity score is larger than thresshold
+    related_ids = ids where similarity_score > threshold
 
-    return ids
+    return related_ids