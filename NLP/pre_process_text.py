409454b1ed4d56a1beb903f9ecb891944c8789d9
kezihlmann <kevin.zihlmann@bluewin.ch>
2022-11-11 21:32:04 +0900

    update database function is only containing the relevant code for deployment.

--- NLP/pre_process_text.py
+++ NLP/pre_process_text.py
@@ -4,15 +4,11 @@ nltk.download('punkt')
 
 import re
 
-def pre_process_text(doc, method):
+def pre_process_text(doc):
     stop_words = nltk.corpus.stopwords.words('english')
 
     # lower case and remove special characters\whitespaces
     doc = re.sub(r'[^a-zA-Z\s]', '', doc, flags=re.I|re.A) # [^a-zA-Z\s] => remove any digits, special characters, symbols etc.
-    if method == "TF_IDF":
-        # include stemming
-        # TODO
-        pass
     doc = doc.lower()
     doc = doc.strip()
     # tokenize document
@@ -23,6 +19,3 @@ def pre_process_text(doc, method):
     doc = ' '.join(filtered_tokens)
     return doc
 
-# sentence = "These are two test sentences to check if this is working correctly. We do this 4 social good. "
-# print(sentence)
-# print(pre_process_text(sentence))
